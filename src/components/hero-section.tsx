89c43224a3208947f53906846ba259969ab38d9a
Bryan Alvarez <bryanalvarez119@gmail.com>
2025-09-28 23:46:56 +0000

    DALE 20+ A LOGOHERO

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
         <div className="mb-4 flex justify-center">
           <BraLogo 
             className="pixel-hover transition-all duration-300"
-            style={{ height: '170px' }}
+            style={{ height: '190px' }}
           />
         </div>
 
