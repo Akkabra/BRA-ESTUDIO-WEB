011d0444d5a3d39f8033f45c11e296d897dc5a2d
Bryan Alvarez <bryanalvarez119@gmail.com>
2025-10-05 05:51:07 +0000

    Necesito preparar el proyecto para un despliegue final y exitoso en Netl

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ5oWc0ZLvAPsHStpjD6at_C0ghOSctbc",
-  authDomain: "studio-2533498908-18d33.firebaseapp.com",
-  projectId: "studio-2533498908-18d33",
-  storageBucket: "studio-2533498908-18d33.appspot.com",
-  messagingSenderId: "902212344137",
-  appId: "1:902212344137:web:94be26e340a8540cdde298",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
